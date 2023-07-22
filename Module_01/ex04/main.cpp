97e8b4ddd581c65921e71008f5c8d8e2fd4f3ee4
Arman <armanmartirosyan81@gmail.com>
2023-07-22 16:50:54 +0400

    ex04:finished

---
+++ Module_01/ex04/main.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void	replaceString(std::string& original,const std::string& toFind, const std::string& replace){
+	size_t	pos = original.find(toFind);
+
+	if (pos == std::string::npos)
+		return ;
+		original = original.substr(0, pos) + replace + original.substr(pos + toFind.length());
+	replaceString(original, toFind, replace);
+	return ;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4){
+		std::cout << "Wrong arguments number" << std::endl;
+		return (1);
+	}
+	std::string 	fileName(argv[1]);
+	std::string 	toFind(argv[2]);
+	std::string		replace(argv[3]);
+	std::ifstream	inputFile;
+	std::ofstream	outputFile;
+	size_t			pos;
+
+	inputFile.open(fileName);
+	if (inputFile.is_open()) {
+		outputFile.open(fileName + ".replace");
+		std::string line;
+		while (std::getline(inputFile, line)) {
+			pos = line.find(toFind);
+			if (pos != std::string::npos)
+				replaceString(line, toFind, replace);
+			outputFile << line << std::endl;
+		}
+		inputFile.close();
+	}
+	else {
+		std::cerr << "Error opening the file." << std::endl;
+		return (1);
+	}
+	return (0);
+}
