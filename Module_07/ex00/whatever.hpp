592780adfd5d369fd6ad7cdf54b95dde8951ca9d
Arman <armanmartirosyan81@gmail.com>
2023-08-27 17:25:17 +0400

    M06:finished;M07:EX02

---
+++ Module_07/ex00/whatever.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void	swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+	return ;
+}
+
+template <typename T>
+T	min(T& a, T& b) {
+	return (a > b ? b : a);
+}
+
+template <typename T>
+T	max(T& a, T& b) {
+	return (a < b ? b : a);
+}
+
+#endif
