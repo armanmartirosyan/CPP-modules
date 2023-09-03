cea33f5652dd39a5d7c3621f39d2825c3393b69b
Arman <armanmartirosyan81@gmail.com>
2023-09-03 14:25:29 +0400

    M08:ex00;

--- Module_08/ex00/easyfind.hpp
+++ Module_08/ex00/easyfind.hpp
@@ -2,16 +2,15 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-# include <algorithm>
 # include <stdexcept>
+# include <algorithm>
 
-template<typename T>
-typename T::iterator	easyFind(T array, int val) {
-	typename T::iterator it = std::find(array.begin(), array.end(), val);
-	if (it != array.end())
-		return (it);
-	else
-		throw std::runtime_error("No such value");
+template <typename T>
+typename T::iterator easyfind(T& container, int val) {
+	typename T::iterator it = std::find(container.begin(), container.end(), val);
+	if (it == container.end())
+		throw std::logic_error("Value is not found.");
+	return (it);
 }
 
 #endif
--- Module_08/ex00/main.cpp
+++ Module_08/ex00/main.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <list>
 #include "easyfind.hpp"
 
-int main (void) {
-	int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	std::vector<int>myVector (array, array + 10);
+# define MAX_VAL 25000
 
-	std::cout << *(easyFind(myVector, 4)) << std::endl;
+int main (void) {
+	std::vector<int>			myVector;
+	std::vector<int>::iterator	itVector;
+	for (size_t i = 0; i < MAX_VAL; i++)
+		myVector.push_back(i);
+	try {
+		itVector = easyfind(myVector, 16241);
+		std::cout << (*itVector) << std::endl;
+	} catch (const std::logic_error& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	std::list<int>				myList;
+	std::list<int>::iterator	itList;
+	for (size_t i = 0; i < MAX_VAL; i++)
+		myList.push_back(i);
+	try {
+		itList = easyfind(myList, 15543);
+		std::cout << (*itList) << std::endl;
+	} catch (const std::logic_error& e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
